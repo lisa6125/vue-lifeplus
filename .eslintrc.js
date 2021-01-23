84b0abe7559326f9558c45aa6bfa1c11f1f301a2
lisa6125 <fxck6125@gmail.com>
2021-01-23 10:44:47 +0800

    去除prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
   parserOptions: {
     parser: 'babel-eslint',
   },
